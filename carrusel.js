c52396ec5b4305d02232509d69d3aab59b4d1538
octiduarte <octiduarte@hotmail.com>
2023-09-25 11:22:04 -0300

    cambio de todo

--- carrusel.js
+++ carrusel.js
@@ -1,25 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const carouselList = document.querySelector(".carousel-list");
-    const carouselItems = document.querySelectorAll(".carousel-item");
-    const prevButton = document.querySelector(".prev-button");
-    const nextButton = document.querySelector(".next-button");
-    let currentIndex = 0;
+const images = ["imagen1.jpg", "imagen2.jpg", "imagen3.jpg"];
+let actualImagen = 0;
 
-    function goToSlide(index) {
-        if (index < 0) {
-            index = carouselItems.length - 1;
-        } else if (index >= carouselItems.length) {
-            index = 0;
-        }
-        carouselList.style.transform = `translateX(-${index * 100}%)`;
-        currentIndex = index;
-    }
+const btnAnterior = document.getElementById("btnAnterior");
+const btnSiguiente = document.getElementById("btnSiguiente");
+const sliderImage = document.getElementById("sliderImage");
 
-    prevButton.addEventListener("click", () => {
-        goToSlide(currentIndex - 1);
-    });
-
-    nextButton.addEventListener("click", () => {
-        goToSlide(currentIndex + 1);
-    });
+btnAnterior.addEventListener("click", () => {
+    actualImagen = (actualImagen - 1 + images.length) % images.length;
+    updateImage();
 });
+
+btnSiguiente.addEventListener("click", () => {
+    actualImagen = (actualImagen + 1) % images.length;
+    updateImage();
+});
+
+function updateImage() {
+    const imageUrl = images[actualImagen];
+    sliderImage.src = imageUrl;
+}
+
+updateImage();
